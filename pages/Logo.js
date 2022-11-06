b69db656c070ddf64e1e2d6d328564f1839ced98
Jingyu Shi (Julie) <julieshi@usc.edu>
2022-11-06 09:49:06 -0800

    Update Logo.js

--- pages/Logo.js
+++ pages/Logo.js
@@ -1,5 +1,5 @@
 
 export const Logo = () => (
-  <a href="/"><img src={"logo.png"} width="70px" style={{marginLeft:"100px"}} /></a>
+  <a href="/"><img src={"logo.png"} width="70px" style={{marginLeft:"100px"}} /> </a>
 
 );
